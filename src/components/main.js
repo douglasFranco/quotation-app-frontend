4c9df312a3749290d086440f0b0024ea8fe97aff
Douglas Robert di Franco <douglas.franco@pampa.compasso>
2020-03-11 00:26:25 -0300

    Fix tabs refs names

--- src/components/main.js
+++ src/components/main.js
@@ -71,19 +71,19 @@ const Main = () => {
       <div className="container">
         <nav>
           <div className="nav nav-tabs" id="nav-tab" role="tablist">
-            <a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Dollar</a>
-            <a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Euro</a>
-            <a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Bitcoin</a>
+            <a className="nav-item nav-link active" id="nav-usd-tab" data-toggle="tab" href="#nav-usd" role="tab" aria-controls="nav-usd" aria-selected="true">Dollar</a>
+            <a className="nav-item nav-link" id="nav-eur-tab" data-toggle="tab" href="#nav-eur" role="tab" aria-controls="nav-eur" aria-selected="false">Euro</a>
+            <a className="nav-item nav-link" id="nav-btc-tab" data-toggle="tab" href="#nav-btc" role="tab" aria-controls="nav-btc" aria-selected="false">Bitcoin</a>
           </div>
         </nav>        
         <div className="tab-content" id="nav-tabContent">
-          <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+          <div className="tab-pane fade show active" id="nav-usd" role="tabpanel" aria-labelledby="nav-usd-tab">
             <Chart currency="USD" data={quotes}/>
           </div>
-          <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+          <div className="tab-pane fade" id="nav-eur" role="tabpanel" aria-labelledby="nav-eur-tab">
             <Chart currency="EUR" data={quotes}/>
           </div>
-          <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+          <div className="tab-pane fade" id="nav-btc" role="tabpanel" aria-labelledby="nav-btc-tab">
             <Chart currency="BTC" data={quotes}/>
           </div>
         </div>      
